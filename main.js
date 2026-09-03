4c323df1148b3121ba53a51912e74659592881f1
Antonio Alcaide <antonio.alcaide.romero@gmail.com>
2026-09-03 22:43:23 +0200

    test-comit
    test-comit

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 Hooks.once('init', () => {
-  console.log("%cdnd5.5-qol-features-by-antua | Módulo inicializado con éxito", "color: red; font-size: 20px; background-color: yellow;");
+  console.log("%cdnd5.5-qol-features-by-antua | Módulo inicializado con éxito", "color: red; font-size: 20px; background-color: yellow;"); 
+  //test
 });
