90d040c46763d9206025549ff7cbe497992650e7
Antonio Alcaide <antonio.alcaide.romero@gmail.com>
2026-09-03 22:49:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 Hooks.once('init', () => {
   console.log("%cdnd5.5-qol-features-by-antua | Módulo inicializado con éxito", "color: red; font-size: 20px; background-color: yellow;"); 
-  //test
+  //test del test
 });
